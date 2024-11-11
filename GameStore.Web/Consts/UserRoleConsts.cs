93dded2df8a1b0ec3ebd066f25b157b07f899e17
Maciej Harbuz <57568383+martinZz8@users.noreply.github.com>
2024-11-11 12:31:35 +0100

    added notes

--- GameStore.Web/Consts/UserRoleConsts.cs
+++ GameStore.Web/Consts/UserRoleConsts.cs
@@ -1,5 +1,6 @@
 ﻿namespace GameStore.Web.Consts
 {
+    // Note: Keep this static class properties updated to match the valid user roles
     public static class UserRoleConsts
     {
         public const string USER = "user";
